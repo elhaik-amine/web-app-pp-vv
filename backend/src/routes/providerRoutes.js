b15b9e052bfb14299c67305a38c1189d2d8b2e64
yikes <gamesmohamed14@gmail.com>
2026-04-16 13:46:58 +0100

    endpoint of provider put

--- backend/src/routes/providerRoutes.js
+++ backend/src/routes/providerRoutes.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const router  = express.Router();
-const { getProviders, getProviderById } = require('../controllers/providerController');
-const { protect, restricted } = require('../middlewares/authMiddleware');
+const express = require("express");
+const router = express.Router();
+const {
+  getProviders,
+  getProviderById,
+  updateProviderProfile,
+} = require("../controllers/providerController");
+const { protect, restricted } = require("../middlewares/authMiddleware");
 
-router.get('/',    protect, restricted, getProviders);
-router.get('/:id', protect, restricted, getProviderById);
+router.get("/", protect, restricted, getProviders);
+router.get("/:id", protect, restricted, getProviderById);
+router.put("/:id", protect, restricted, updateProviderProfile);
 
 module.exports = router;
