250a3dee04300e22a58b61ebd7c4e1efea2aee0a
yikes <gamesmohamed14@gmail.com>
2026-04-17 14:28:10 +0100

    availability of a provider

--- backend/src/routes/providerRoutes.js
+++ backend/src/routes/providerRoutes.js
@@ -4,10 +4,12 @@ const {
   getProviders,
   getProviderById,
   updateProviderProfile,
+  getProviderWeekAvailability,
 } = require("../controllers/providerController");
 const { protect, restricted } = require("../middlewares/authMiddleware");
 
 router.get("/", protect, restricted, getProviders);
+router.get("/:id/availability", protect, getProviderWeekAvailability); // 7-day slot availability
 router.get("/:id", protect, restricted, getProviderById);
 router.put("/:id", protect, restricted, updateProviderProfile);
 
